bdc18eb368da1ca74816dc308fa0a4fe4bbee242
David Salas <davidlivingrooms@gmail.com>
2016-11-01 22:28:10 -0600

    adding sound files

---
+++ android/app/src/main/java/com/mtgpal/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mtgpal;
+
+import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.cmcewen.blurview.BlurViewPackage;
+import com.zmxv.RNSound.RNSoundPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mtgPal";
+    }
+}
